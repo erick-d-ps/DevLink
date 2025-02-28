3f543a470114d389090350e88503ab5e09fd7f65
deverick <dncellassistec@gmail.com>
2025-02-28 16:53:53 -0300

    funcionalidades da pagina admin

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,7 +1,26 @@
-import { useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { Input } from "../../components/input";
-import { FiTrash } from 'react-icons/fi'
+import { FiTrash } from "react-icons/fi";
+
+import { db } from "../../services/firebaseConnection";
+import {
+  addDoc,
+  collection,
+  onSnapshot,
+  query,
+  orderBy,
+  doc,
+  deleteDoc,
+} from "firebase/firestore";
+
+interface LinkProps {
+  id: string;
+  name: string;
+  url: string;
+  bg: string;
+  color: string;
+}
 
 export function Admin() {
   const [nameInput, setNameInput] = useState("");
@@ -9,11 +28,70 @@ export function Admin() {
   const [textColorInput, setTextColorInput] = useState("#f2f3f4");
   const [backgroundColorInput, setBackgroundColorInput] = useState("#000000");
 
+  const [links, setLinks] = useState<LinkProps[]>([]);
+
+  useEffect(() => {
+    const linksRef = collection(db, "Links");
+    const queryRef = query(linksRef, orderBy("created", "asc"));
+
+    const unsub = onSnapshot(queryRef, (snapshot) => {
+      let lista = [] as LinkProps[];
+
+      snapshot.forEach((doc) => {
+        lista.push({
+          id: doc.id,
+          name: doc.data().name,
+          url: doc.data().url,
+          bg: doc.data().bg,
+          color: doc.data().color,
+        });
+      });
+
+      setLinks(lista);
+    });
+
+    return () => {
+      unsub();
+    };
+  }, []);
+
+  function handleRegister(e: FormEvent) {
+    e.preventDefault();
+
+    if (nameInput === "" || urlInput === "") {
+      alert("Preencha todos os campos!");
+    }
+
+    addDoc(collection(db, "Links"), {
+      name: nameInput,
+      url: urlInput,
+      bg: backgroundColorInput,
+      color: textColorInput,
+      created: new Date(),
+    })
+      .then(() => {
+        setNameInput("");
+        setUrlInput("");
+        console.log("CADASTRADO COM SUCESSO!");
+      })
+      .catch((error) => {
+        console.log("ERRO AO CADASTRAR!" + error);
+      });
+  }
+
+  async function handleDeletLink(id: string) {
+    const docRef = doc(db, "Links", id);
+    await deleteDoc(docRef);
+  }
+
   return (
     <div className="flex items-center flex-col min-h-screen pb-7 px-2">
       <Header />
 
-      <form className="flex flex-col mt-8 mb-3 w-full max-w-xl">
+      <form
+        className="flex flex-col mt-8 mb-3 w-full max-w-xl"
+        onSubmit={handleRegister}
+      >
         <label className="text-white font-medium mt-2 mb-2">Nome do Link</label>
         <Input
           placeholder="Digit o nome do Link..."
@@ -76,21 +154,24 @@ export function Admin() {
           Cadastrar
         </button>
       </form>
-      <h2 className="font-bold text-white mb-4 text-2xl">
-        Meus Links
-      </h2>
-      <article className=" flex items-center justify-between w-11/12 max-w-xl rounded px-3 py-2 mb-2 font-medium select-none "
-      style={{backgroundColor:"#2563EB", color:"#fff"}}
-      >
-        <p>Canal no Youtube</p>
-        <div>
+      <h2 className="font-bold text-white mb-4 text-2xl">Meus Links</h2>
+      {links.map((item) => (
+        <article
+          key={item.id}
+          className=" flex items-center justify-between w-11/12 max-w-xl rounded px-3 py-2 mb-2 font-medium select-none "
+          style={{ backgroundColor: item.bg, color: item.color }}
+        >
+          <p>{item.name}</p>
+          <div>
             <button
-            className="border border-dashed p-1 cursor-pointer bg-neutral-700"
+              className="border border-dashed p-1 cursor-pointer bg-neutral-700"
+              onClick={ () => handleDeletLink(item.id)}
             >
-                <FiTrash size={18} color="#fff"/>
+              <FiTrash size={18} color="#fff" />
             </button>
-        </div>
-      </article>
+          </div>
+        </article>
+      ))}
     </div>
   );
 }
